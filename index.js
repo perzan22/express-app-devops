509be4ace58ece4074131c9af5a91500ac3e1373
olafperzanowski <olafperzanowski@gmail.com>
2025-01-20 21:22:40 +0100

    Zmiana po watchtower

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Zmiana działa</h1>')
+  res.send('<h1>Zmiana po watchtower</h1>')
 })
 
 const PORT = 8080
