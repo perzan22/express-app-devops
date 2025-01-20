db013ecbd4b46e49d921c659930d87b964da51da
olafperzanowski <olafperzanowski@gmail.com>
2025-01-20 21:32:41 +0100

    Zmiany bez restartowania

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,3 @@ FROM node:16
 COPY . .
 
 RUN npm install
-
-CMD node index.js
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Zmiana po watchtower</h1>')
+  res.send('<h1>Zmiana wykonana bez uruchamiania na nowo kontenera</h1>')
 })
 
 const PORT = 8080
