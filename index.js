4351d60c2a24ad258fafd0659f39006711a2076a
olafperzanowski <olafperzanowski@gmail.com>
2025-01-20 20:11:44 +0100

    zmiana 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>How are you</h1>')
+  res.send('<h1>Zmiana działa</h1>')
 })
 
 const PORT = 8080
